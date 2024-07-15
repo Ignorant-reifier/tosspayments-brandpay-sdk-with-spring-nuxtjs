801c16525b2d2f3c8bc96a14d18bbe8f52933e5d
oseongryu <oseongryu@gmail.com>
2024-07-15 11:30:40 +0900

    fix: modify data

--- brandpay-back/src/main/resources/sql/data.sql
+++ brandpay-back/src/main/resources/sql/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070501', '햄버거세트', 5000, '01. 풍미 가득한 햄버거와 바삭한 감자튀김 세트', 'https://loremflickr.com/350/350/hamberger');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070502', '햄버거세트', 1000, '02. 달콤하고 부드러운 호박 맛이 가득한 쿠키', 'https://loremflickr.com/350/350/Pumpkin,Cookie');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070503', '햄버거세트', 2000, '03. 시원하고 청량감 넘치는 탄산 음료 사이다', 'https://loremflickr.com/350/350/coke,cola,soda');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070504', '햄버거세트', 15000, '04. 얇은 도우에 햄과 치즈를 얹어 구워낸 작은 피자', 'https://loremflickr.com/350/350/pizza');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070505', '햄버거세트', 7000, '05. 얇은 도우에 햄과 치즈를 얹어 구운 미니피자', 'https://loremflickr.com/350/350/PepperoniPizza');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070506', '햄버거세트', 10000, '06. 신선한 브로콜리와 크림이 어우러진 파스타', 'https://loremflickr.com/350/350/pasta');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070507', '햄버거세트', 20000, '07. 간편하게 즐길 수 있는 신선한 편의점 초밥', 'https://loremflickr.com/350/350/sushi,japan');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070508', '햄버거세트', 500, '08. 달콤한 빨간색 사탕, 어린이들이 좋아하는 맛', 'https://loremflickr.com/350/350/candy,Chocolate');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070509', '햄버거세트', 1000000, '09. 피자빵과 신선한 샐러드가 어우러진 한 접시', 'https://loremflickr.com/350/350/pizza,pizzabread');
-INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070510', '햄버거세트', 30000, '10. 모카 향이 가득한 부드러운 크림 케이크', 'https://loremflickr.com/350/350/Chocolate,cake');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070501', '햄버거세트', 10000, '01. 풍미 가득한 햄버거와 바삭한 감자튀김 세트', 'https://loremflickr.com/350/350/hamberger');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070502', '호박쿠키', 2000, '02. 달콤하고 부드러운 호박 맛이 가득한 쿠키', 'https://loremflickr.com/350/350/Pumpkin,Cookie');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070503', '음료수', 3000, '03. 시원하고 청량감 넘치는 탄산 음료 사이다', 'https://loremflickr.com/350/350/coke,cola,soda');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070504', '미니피자', 8000, '04. 얇은 도우에 햄과 치즈를 얹어 구워낸 작은 피자', 'https://loremflickr.com/350/350/pizza');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070505', '페페로니피자', 11000, '05. 얇은 도우에 햄과 치즈를 얹어 구운 미니피자', 'https://loremflickr.com/350/350/PepperoniPizza');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070506', '브콜리파스타', 12000, '06. 신선한 브로콜리와 크림이 어우러진 파스타', 'https://loremflickr.com/350/350/pasta');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070507', '편의점초밥', 9900, '07. 간편하게 즐길 수 있는 신선한 편의점 초밥', 'https://loremflickr.com/350/350/sushi,japan');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070508', '사탕-빨강', 500, '08. 달콤한 빨간색 사탕, 어린이들이 좋아하는 맛', 'https://loremflickr.com/350/350/candy,Chocolate');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070509', '피자빵샐러드', 13000, '09. 피자빵과 신선한 샐러드가 어우러진 한 접시', 'https://loremflickr.com/350/350/pizza,pizzabread');
+INSERT INTO PRODUCT (PRD_ID, PRD_NM, PRD_PRICE, PRD_DESC, PRD_IMG) VALUES ('2024070510', '모카크림케이크', 6500, '10. 모카 향이 가득한 부드러운 크림 케이크', 'https://loremflickr.com/350/350/Chocolate,cake');
 
 INSERT INTO CUSTOMER (customer_key, customer_name, customer_email) VALUES ('toss_brandpay_tester_1', '전테스터', 'jeon_tester_1@test.com');
 INSERT INTO CUSTOMER (customer_key, customer_name, customer_email) VALUES ('toss_brandpay_manager_2', '유매니저', 'ryu_manager_2@gamil.com');
